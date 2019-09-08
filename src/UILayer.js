557d1371d030b2688c056cd2e6f8cae24ac97556
mrpodkalicki <mr.podkalicki.adrian94@gmail.com>
2019-09-08 23:14:44 +0200

    card apperance,adding list cards to dom,code stylization

--- src/UILayer.js
+++ src/UILayer.js
@@ -2,17 +2,17 @@ import {SearchingMovies} from "./SWAPIService.js"
 // ******* UI Layer *********
 
 // DOM nodes
-let $moviesList = document.querySelector(".article__second");
+let $moviesList = document.querySelector(".main-section__content-items");
 let $movieInput = document.querySelector("#movie-input");
 let $searchButton = document.querySelector("#movie-search");
-let $articleFirst = document.querySelector(".header__nothingFinded");
+let $nothingFinded = document.querySelector(".main-section__content-input__caption-nothing-finded");
 
 const createCardTitle=(title)=>{
     const divCardContent = document.createElement("div");
-    divCardContent.className = "card__content card__titleContent card__titleContent--position card__titleContent--yelow";
+    divCardContent.className = "b-card__content b-card__header b-card__header--position";
     const h1=document.createElement("h1");
     h1.innerText = title;
-    h1.className = "card__header card__headerName";
+    h1.className = "b-card__header__caption";
     divCardContent.appendChild(h1);
     return divCardContent;
 };
@@ -39,68 +39,56 @@ const createMovieCard=(movie,classNamebackImange)=>{
     const director = movie["director"];
     const producer=movie["producer"];
     const realaseDate = movie["release_date"];
-    const classNameDivDirector = "card__content card__directorContent card__directorContent--position";
-    const classNameDivProducer = " card__content card__producerContent card__producerContent--position"
-    const classNameDivDate = "card__content card__dateContent card__dateContent--position";
-    const directClassPheader = "card__header card__directorHeader card__header--red";
-    const directClassPName = "card__name card__directorName card__name--white"
-    const producerClassPheader = "card__header card__producer card__header--red";
-    const producerClassPName = "card__name card__producerName card__name--white"
-    const dateClassPheader = "card__header card_date card__header--red";
-    const dateClassPName = "card__name card_dateRealase card__name--white"
+    const classNameDivDirector = "b-card__content card__content-director card__content-director--position";
+    const classNameDivProducer = " b-card__content card__content-producer  card__content-producer--position"
+    const classNameDivDate = "b-card__content card__content-date card__content-date--position";
+    const directClassPheader = "card__subtitle card__directorHeader card__subtitle--red";
+    const directClassPName = "card__subtitle__name card__directorName card__subtitle__name--white"
+    const producerClassPheader = "card__subtitle card__producer card__subtitle--red";
+    const producerClassPName = "card__subtitle__name card__producerName card__subtitle__name--white"
+    const dateClassPheader = "card__subtitle card_date card__subtitle--red";
+    const dateClassPName = "card__subtitle__name card_dateRealase card__subtitle__names--white"
     const card= document.createElement("div");
     card.className = "card  card--shadow card__backgroundFirst--image " + `${classNamebackImange}`;
-
     card.appendChild(createDivTransparency());
     card.appendChild(createCardTitle(title));
     card.appendChild(createCardContent("Director:", director, classNameDivDirector, directClassPheader, directClassPName));
     card.appendChild(createCardContent("Producer:", producer, classNameDivProducer, producerClassPheader, producerClassPName));
     card.appendChild(createCardContent("Realese Date:", realaseDate, classNameDivDate, dateClassPheader, dateClassPName));
-    // console.log(card, "card ")
     return  card
      
 }
-    
-
 // UI functions
 let movies = [];
 function render() {
     if (movies[0] == "Nothing finded") {
         $moviesList.innerHTML = "";
-        $articleFirst.innerText="nothig Finded"
+        $nothingFinded.innerText="nothig Finded"
     }else{
-        const classBackImage = ["card__backgroundFirst--image", "card__backgroundSecond--image", "card__backgroundThird--image", "card__backgroundFourth--image", "card__backgroundFifth--image", "card__backgroundSixth--image"];
-
+        const classBackImage = ["card__background-first--image", "card__background-second--image", "card__background-third--image", "card__background-fourth--image", "card__background-fifth--image", "card__background-sixth--image"];
         const listCard = document.createElement("ul");
-        listCard.className = "list__cards";
+        listCard.className = "main-section__content-items__list__cards";
         let i = 0;
         movies.forEach(movie => {
-            
-            console.log(i)
             createMovieCard(movie);
             listCard.appendChild(createMovieCard(movie, classBackImage[i]));
             if (i == classBackImage.length-1) {
                 i = 0;
-                console.log("siema")
             } else {
                 i++;
             };
-            
-
         })
-        // console.log(listCard,"listcard");
         $moviesList.innerHTML = "";
-         $articleFirst.innerText = "";
+        $nothingFinded.innerText = "";
         $moviesList.appendChild(listCard);
-
-        
-
     };
    
 }
 $searchButton.addEventListener("click", () => {
     SearchingMovies($movieInput.value).then(resolve => {
+        console.log(resolve);
         if (resolve =="Nothing finded") {
+            
             movies[0] = resolve;
         }else{
               movies = resolve;
